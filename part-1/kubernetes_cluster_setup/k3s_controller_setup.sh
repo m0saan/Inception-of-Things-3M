a5e29e8139a4985de0c4739621982ba5cf717a6b
m0saan <moboustt@student.1337.ma>
2024-03-19 17:47:45 +0000

    refactoring :p -- part-1 [✅]

--- part-1/kubernetes_cluster_setup/k3s_controller_setup.sh
+++ part-1/kubernetes_cluster_setup/k3s_controller_setup.sh
@@ -8,29 +8,27 @@ SERVER_IP="$1"
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
+LOADING_SYMBOL="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
 
 # Create directory for configurations (if not exist)
-sudo mkdir -p /vagrant/.confs
+sudo mkdir -p /vagrant/.config
 
 # Create a file to store the server token
-sudo touch /vagrant/.confs/server_token.txt
+sudo touch /vagrant/.config/k3s_server_token.txt
 
 # Install K3s
 # Set K3s server options
-echo -e "${CYAN}Installing K3s server...${NC}"
+echo -e "${CYAN}${LOADING_SYMBOL} Installing K3s server...${NC}"
 
 INSTALL_K3S_EXEC="--bind-address=${SERVER_IP} --flannel-iface=eth1"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" sh - > /dev/null && \
     echo -e "${GREEN}K3s server installed successfully.${NC}"
 
 # Copy server token to shared directory
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/.confs/server_token.txt
-
-# Copy K3s configuration file to shared directory
-sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/.confs/
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/.config/k3s_server_token.txt
 
 # Install net-tools package for ifconfig command (optional)
-echo -e "${CYAN}Installing net-tools package...${NC}"
+echo -e "${CYAN}${LOADING_SYMBOL} Installing net-tools package...${NC}"
 sudo apt-get install -y net-tools >/dev/null && \
     echo -e "${GREEN}Net-tools package installed.${NC}"
 
--- part-1/kubernetes_cluster_setup/k3s_worker_setup.sh
+++ part-1/kubernetes_cluster_setup/k3s_worker_setup.sh
@@ -7,8 +7,6 @@ SERVER_IP=$1
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
-
-# Loading symbol
 LOADING_SYMBOL="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
 
 echo -e "${CYAN}Starting K3s Agent installation...${NC}"
@@ -18,20 +16,20 @@ SERVER_PORT=6443
 SERVER_URL="https://${SERVER_IP}:${SERVER_PORT}"
 
 # K3s token file location
-K3S_TOKEN_FILE="/vagrant/.confs/server_token.txt"
+K3S_TOKEN_FILE="/vagrant/.config/k3s_server_token.txt"
 
 # Install K3s Agent
 echo -e "${CYAN}${LOADING_SYMBOL} Installing K3s Agent...${NC}"
 curl -sfL https://get.k3s.io | K3S_URL=${SERVER_URL} K3S_TOKEN_FILE=${K3S_TOKEN_FILE} INSTALL_K3S_EXEC="--flannel-iface=eth1" sh - > /dev/null && \
     echo -e "${GREEN}K3s Agent is Running.${NC}"
 
-# Clean up
-echo -e "${CYAN}Cleaning up...${NC}"
-sudo rm -rf /vagrant/.confs
-
 # Install net-tools package (optional)
 echo -e "${CYAN}${LOADING_SYMBOL} Installing net-tools package...${NC}"
 sudo apt install -y net-tools > /dev/null && \
     echo -e "${GREEN}Net-tools package installed.${NC}"
+
+# Clean up
+echo -e "${CYAN}Cleaning up...${NC}"
+sudo rm -rf /vagrant/.config
 
 echo -e "${GREEN}K3s Agent setup completed.${NC}"
