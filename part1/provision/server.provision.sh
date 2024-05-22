29e78aedaf44ea067bc89a3e5fe47bddd75f5183
ELORCHI <elorchie@gmail.com>
2024-05-22 20:08:45 +0100

    part1 tested on linux

--- part1/provision/server.provision.sh
+++ part1/provision/server.provision.sh
@@ -2,6 +2,9 @@ SERVER_IP="$1"
 INSTALL_K3S_EXEC="--node-ip ${SERVER_IP} --write-kubeconfig-mode=644 --log /vagrant/logs/server.logs"
 curl -fsL https://get.k3s.io | INSTALL_K3S_EXEC="$INSTALL_K3S_EXEC" sh -
 echo $INSTALL_K3S_EXEC
-cp /etc/rancher/k3s/k3s.yaml /vagrant/provision/config
-cp /var/lib/rancher/k3s/server/token /vagrant/provision/token
-
+while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
+  echo "Waiting for /etc/rancher/k3s/k3s.yaml to be available..."
+  sleep 5
+done
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/provision/config
+sudo cp /var/lib/rancher/k3s/server/token /vagrant/provision/token
